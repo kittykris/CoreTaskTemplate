b45bd124c318ad98fe449a5ac698d74450f178ce
Kristina <kitty_kris@bk.ru>
2021-12-16 19:27:06 +0300

    make getJDBCConnection - singleton

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -9,15 +9,16 @@ public class Util {
     private static final String MYSQL_URL = "jdbc:mysql://localhost:3306/mydb";
     private static final String USER = "root";
     private static final String PASSWORD = "12345";
-
+    private static Connection connection;
 
     public static Connection getJDBCConnection() {
-        Connection connection = null;
-        try {
-            Class.forName(MYSQL_DRIVER);
-            connection = DriverManager.getConnection(MYSQL_URL, USER, PASSWORD);
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("Problem with connection");
+        if (connection == null) {
+            try {
+                Class.forName(MYSQL_DRIVER);
+                connection = DriverManager.getConnection(MYSQL_URL, USER, PASSWORD);
+            } catch (ClassNotFoundException | SQLException e) {
+                System.out.println("Problem with connection");
+            }
         }
         return connection;
     }
